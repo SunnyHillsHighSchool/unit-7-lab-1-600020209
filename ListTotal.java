f33df0f856d6f1bf65f3fe531493d6dad3cc2d24
Joon Kyo Kyo <600020209@fjuhsd.org>
2021-01-28 23:07:58 +0000

    Completed method total in ListTotal class

--- ListTotal.java
+++ ListTotal.java
@@ -10,6 +10,15 @@ public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
+  //Initialize int variable totalSum with value 0
+  int totalSum = 0;
+  //Start for loop with counter variable i, while i is less than the size of the list, and increment i by one after each iteration
+  for(int i = 0; i < ray.size(); i++){
+    //Get the value of the element at index i of the list and add it to total
+    totalSum += ray.get(i);
+  //End loop
+  }
+  //Return total
+  return totalSum;
  }
 }
